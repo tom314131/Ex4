2713326191e7b88e2c5dc6f2a62873705661df34
tom314131 <45948148+tom314131@users.noreply.github.com>
2019-06-14 20:22:35 +0300

    Starting in creating a tcp connection with the simulator

--- app/src/main/java/com/example/ex4/LoginActivity.java
+++ app/src/main/java/com/example/ex4/LoginActivity.java
@@ -3,8 +3,12 @@ package com.example.ex4;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,9 +19,18 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void ShowMessage(View view){
-        EditText editText = (EditText)findViewById(R.id.port_text);
+        EditText ip = (EditText)findViewById(R.id.ip_text);
+        EditText port= (EditText)findViewById(R.id.port_text);
+        try {//here you must put your computer's IP address.
+            String st = "4324";
+            InetAddress serverAddr = InetAddress.getByName(ip.getText().toString());
+            //create a socket to make the connection with the server
+            Socket socket= new Socket(serverAddr, Integer.parseInt(port.getText().toString()));
+        } catch (Exception e){
+            Log.e("TCP", "C: Error", e);
+        }
         Intent intent=new Intent(this,JoystickActivity.class);
-        String msg=editText.getText().toString();intent.putExtra("message",msg);
+        String msg=ip.getText().toString();intent.putExtra("message",msg);
         startActivity(intent);
     }
 }
